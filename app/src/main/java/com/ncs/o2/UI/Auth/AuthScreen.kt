51a0fa5c9a8bde20ec0fe0854c13c4e011cc320b
Armax | Alok <arpitmaurya01codechef@gmail.com>
2023-06-20 23:07:36 +0530

    Updates

---
+++ app/src/main/java/com/ncs/o2/UI/Auth/AuthScreen.kt
@@ -0,0 +1,12 @@
+package com.ncs.o2.UI.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ncs.o2.R
+
+class AuthScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_screen)
+    }
+}
---
+++ app/src/main/java/com/ncs/o2/UI/Auth/AuthViewModel.kt
@@ -0,0 +1,22 @@
+package com.ncs.o2.UI.Auth
+
+import androidx.lifecycle.ViewModel
+
+/*
+File : AuthViewModel.kt -> com.ncs.o2.UI.Auth
+Description : ViewModel for auth 
+
+Author : Alok Ranjan (VC uname : apple)
+Link : https://github.com/arpitmx
+From : Bitpolarity x Noshbae (@Project : O2 Android)
+
+Creation : 11:00 pm on 20/06/23
+
+Todo >
+Tasks CLEAN CODE : 
+Tasks BUG FIXES : 
+Tasks FEATURE MUST HAVE : 
+Tasks FUTURE ADDITION :
+*/
+class AuthViewModel : ViewModel() {
+}
