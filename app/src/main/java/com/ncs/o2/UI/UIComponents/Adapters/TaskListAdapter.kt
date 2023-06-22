8851a2411ee9cfc5fd4aafe6d84bcd4e4dfeb0ad
mohit <mohitsingh732@yahoo.com>
2023-06-22 20:47:01 +0530

    Task Detail list revamp

--- app/src/main/java/com/ncs/o2/UI/UIComponents/Adapters/TaskListAdapter.kt
+++ app/src/main/java/com/ncs/o2/UI/UIComponents/Adapters/TaskListAdapter.kt
@@ -1,11 +1,22 @@
 package com.ncs.o2.UI.UIComponents.Adapters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.graphics.Paint
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.ncs.o2.Domain.Models.Task
+import com.ncs.o2.Domain.Utility.ExtensionsUtil.gone
 import com.ncs.o2.Domain.Utility.ExtensionsUtil.setOnClickFadeInListener
+import com.ncs.o2.R
 import com.ncs.o2.databinding.TaskItemBinding
 
 /*
@@ -37,17 +48,54 @@ class TaskListAdapter(
 
 
         fun bind(task: Task) {
+            Glide.with(binding.root)
+                .load(task.ASSIGNEE_DP_URL)
+                .listener(object : RequestListener<Drawable> {
 
-            binding.taskDuration.text= task.DURATION
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.gone()
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.gone()
+                        return false
+                    }
+                })
+                .encodeQuality(80)
+                .override(40,40)
+                .apply(
+                    RequestOptions().
+                    diskCacheStrategy(DiskCacheStrategy.ALL)
+
+                )
+                .error(R.drawable.profile_pic_placeholder)
+                .into(binding.asigneeDp)
+
+            if (task.isCompleted){
+                binding.taskId.paintFlags=binding.taskId.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                binding.taskTitle.paintFlags=binding.taskTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+
+
+            }
+            binding.taskDuration.text= "about "+task.DURATION+" hours ago"
             binding.taskId.text = task.ID
-            binding.taskStatus.text = task.getStatusString()
             binding.taskTitle.text = task.TITLE
-
             binding.difficulty.text = task.getDifficultyString()
-            binding.difficulty.setTextColor(task.getDifficultyColor())
+            binding.difficulty.setBackgroundColor(task.getDifficultyColor())
 
-            binding.priority.text = task.PRIORITY.toString()
-            binding.priority.setBackgroundColor(task.getPriorityColor())
+
 
         }
 
